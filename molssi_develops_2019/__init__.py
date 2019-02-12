beeef4f2a1ea29133610ae647f64b6cf2acda1bd
Fang Liu <fangliu.ustc@gmail.com>
2019-02-12 09:34:45 -0500

    Initial commit after CMS Cookiecutter creation, version 1.0

---
+++ molssi_develops_2019/__init__.py
@@ -0,0 +1,18 @@
+"""
+molssi_develops_2019
+A short description of the project.
+"""
+
+# Make Python 2 and 3 imports work the same
+# Safe to remove with Python 3-only code
+from __future__ import absolute_import
+
+# Add imports here
+from .molssi_develops_2019 import *
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
---
+++ molssi_develops_2019/tests/test_molssi_develops_2019.py
@@ -0,0 +1,12 @@
+"""
+Unit and regression test for the molssi_develops_2019 package.
+"""
+
+# Import package, test suite, and other packages as needed
+import molssi_develops_2019
+import pytest
+import sys
+
+def test_molssi_develops_2019_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "molssi_develops_2019" in sys.modules
